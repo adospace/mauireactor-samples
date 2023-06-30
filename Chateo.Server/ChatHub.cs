6cd4e5dc29515fc3601cce0bdd32f623ff484dee
Adolfo Marinucci <adospace@msn.com>
2023-06-30 19:24:00 +0200

    Chateo initial commit

---
+++ Chateo.Server/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Chateo.Server.Models;
+using Chateo.Shared;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chateo.Server;
+
+
+public class ChatHub : Hub
+{
+    private readonly List<User> _users = new ();
+    
+
+    public async Task RegisterUser(UserCreateModel createModel)
+    {
+        _users.Add(new User
+        {
+            Id = createModel.Id,
+            FirstName = createModel.FirstName,
+            LastName = createModel.LastName
+        });
+
+        var caller = Clients.Caller;
+
+        
+
+        await Clients.All.SendAsync("NewUser", new UserUpdatedModel(createModel.Id, createModel.FirstName, createModel.LastName));
+
+    }
+
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
---
+++ Chateo.Shared/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Chateo.Shared;
+
+public record UserCreateModel(Guid Id, string FirstName, string LastName);
+
+public record UserUpdatedModel(Guid Id, string FirstName, string LastName);
+
+public record MessageCreateModel(Guid Id, Guid FromUserId, Guid ToUserId, string Content);
