f07779485fe526f7a848b9ab3d5b528498517e6d
Adolfo Marinucci <adospace@msn.com>
2023-09-30 16:05:54 +0200

    FlyoutPageTestApp

---
+++ Controls/FlyoutPageTestApp/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using MauiReactor;
+using FlyoutPageTestApp.Pages;
+
+
+namespace FlyoutPageTestApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiReactorApp<MainPage>(app =>
+            {
+                app.AddResource("Resources/Styles/Colors.xaml");
+                app.AddResource("Resources/Styles/Styles.xaml");
+            })
+#if DEBUG
+            .EnableMauiReactorHotReload()
+#endif
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+            });
+
+        return builder.Build();
+    }
+}
+
---
+++ Controls/FlyoutPageTestApp/Pages/MainPage.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiReactor;
+
+namespace FlyoutPageTestApp.Pages;
+
+enum PageType
+{
+    Contacts,
+
+    TodoList,
+
+    Reminders
+}
+
+class MainPageState
+{
+    public PageType CurrentPageType { get; set; }
+}
+
+class MainPage : Component<MainPageState>
+{
+    public override VisualNode Render()
+    {
+        return new FlyoutPage
+        {
+            DetailPage()
+        }
+        .Flyout(new FlyoutMenuPage()
+            .OnPageSelected(pageType => SetState(s => s.CurrentPageType = pageType))
+        )
+        ;
+    }
+
+    VisualNode DetailPage()
+    {
+        return State.CurrentPageType switch
+        {
+            PageType.Contacts => new ContactsPage(),
+            PageType.TodoList => new TodoListPage(),
+            PageType.Reminders => new RemindersPage(),
+            _ => throw new InvalidOperationException(),
+        };
+    }
+
+}
+
+class FlyoutMenuPage : Component
+{
+    private Action<PageType> _selectAction;
+
+    public FlyoutMenuPage OnPageSelected(Action<PageType> action)
+    {
+        _selectAction = action;
+        return this;
+    }
+
+    public override VisualNode Render()
+    {
+        return new ContentPage("Personal Organiser")
+        {
+            new CollectionView()
+                .ItemsSource(Enum.GetValues<PageType>(), pageType =>
+                    new Label(pageType.ToString())
+                        .Margin(10,5)
+                        .VCenter())
+                .SelectionMode(Microsoft.Maui.Controls.SelectionMode.Single)
+                .OnSelected<CollectionView, PageType>(pageType => _selectAction?.Invoke(pageType))
+        };
+    }
+}
+
+class ContactsPage : Component
+{
+    public override VisualNode Render()
+        => new NavigationPage
+        {
+            new ContentPage("Contacts")
+            {
+                new Label("Contacts")
+                    .VCenter()
+                    .HCenter()
+            }
+        };
+}
+
+class TodoListPage : Component
+{
+    public override VisualNode Render()
+        => new NavigationPage
+        {
+            new ContentPage("TodoList")
+            {
+                new Label("TodoList")
+                    .VCenter()
+                    .HCenter()
+            }
+        };
+}
+
+class RemindersPage : Component
+{
+    public override VisualNode Render() =>
+        new NavigationPage
+        {
+            new ContentPage("Reminders")
+            {
+                new Label("Reminders")
+                    .VCenter()
+                    .HCenter()
+            }
+        };
+}
