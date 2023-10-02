9c777142b61675f7cec8bdcd8f4530acdb67fd55
Adolfo Marinucci <adospace@msn.com>
2023-10-02 22:45:51 +0200

    IndicatorView test app

---
+++ Controls/IndicatorViewTestApp/Pages/MainPage.cs
@@ -0,0 +1,80 @@
+﻿using MauiReactor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndicatorViewTestApp.Pages;
+public class Animal
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Details { get; set; }
+    public string ImageUrl { get; set; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+class MainPageState
+{
+    public Animal[] Animals { get; set; }
+}
+
+class MainPage : Component<MainPageState>
+{
+    protected override void OnMounted()
+    {
+        State.Animals = new Animal[] 
+        {
+            new Animal
+            {
+                Name = "American Black Bear",
+                Location = "North America",
+                Details = "The American black bear is a medium-sized bear native to North America. It is the continent's smallest and most widely distributed bear species. American black bears are omnivores, with their diets varying greatly depending on season and location. They typically live in largely forested areas, but do leave forests in search of food. Sometimes they become attracted to human communities because of the immediate availability of food. The American black bear is the world's most common bear species.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/0/08/01_Schwarzbär.jpg"
+            },
+            new Animal
+            {
+                Name = "Asian Black Bear",
+                Location = "Asia",
+                Details = "The Asian black bear, also known as the moon bear and the white-chested bear, is a medium-sized bear species native to Asia and largely adapted to arboreal life. It lives in the Himalayas, in the northern parts of the Indian subcontinent, Korea, northeastern China, the Russian Far East, the Honshū and Shikoku islands of Japan, and Taiwan. It is classified as vulnerable by the International Union for Conservation of Nature (IUCN), mostly because of deforestation and hunting for its body parts.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Ursus_thibetanus_3_%28Wroclaw_zoo%29.JPG/180px-Ursus_thibetanus_3_%28Wroclaw_zoo%29.JPG"
+            },
+            new Animal
+            {
+                Name = "Brown Bear",
+                Location = "Northern Eurasia & North America",
+                Details = "The brown bear is a bear that is found across much of northern Eurasia and North America. In North America the population of brown bears are often called grizzly bears. It is one of the largest living terrestrial members of the order Carnivora, rivaled in size only by its closest relative, the polar bear, which is much less variable in size and slightly larger on average. The brown bear's principal range includes parts of Russia, Central Asia, China, Canada, the United States, Scandinavia and the Carpathian region, especially Romania, Anatolia and the Caucasus. The brown bear is recognized as a national and state animal in several European countries.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Kamchatka_Brown_Bear_near_Dvuhyurtochnoe_on_2015-07-23.jpg/320px-Kamchatka_Brown_Bear_near_Dvuhyurtochnoe_on_2015-07-23.jpg"
+            },
+            new Animal
+            {
+                Name = "Grizzly-Polar Bear Hybrid",
+                Location = "Canadian Artic",
+                Details = "A grizzly–polar bear hybrid is a rare ursid hybrid that has occurred both in captivity and in the wild. In 2006, the occurrence of this hybrid in nature was confirmed by testing the DNA of a unique-looking bear that had been shot near Sachs Harbour, Northwest Territories on Banks Island in the Canadian Arctic. The number of confirmed hybrids has since risen to eight, all of them descending from the same female polar bear.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Grolar.JPG/276px-Grolar.JPG"
+            },
+        };
+
+        base.OnMounted();
+    }
+
+
+    public override VisualNode Render()
+    {
+        return new ContentPage
+        {
+            new IndicatorView()
+                .ItemsSource(State.Animals)
+                .IndicatorColor(Colors.Red)
+                .IndicatorSize(14)
+                .IndicatorsShape(MauiControls.IndicatorShape.Circle)
+                .VCenter()
+                .HCenter()
+        };
+    }
+}
