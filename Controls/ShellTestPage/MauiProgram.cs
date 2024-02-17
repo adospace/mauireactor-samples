a984e4e1e85d0a318a14ea1488b83bdb28823528
Adolfo Marinucci <adospace@msn.com>
2024-02-17 15:44:35 +0100

    Added sample page for Shell Tab using custom icon font

--- Controls/ShellTestPage/MauiProgram.cs
+++ Controls/ShellTestPage/MauiProgram.cs
@@ -10,18 +10,26 @@ namespace ShellTestPage
         {
             var builder = MauiApp.CreateBuilder();
             builder
-                .UseMauiReactorApp<MainPage7>(app =>
+                .UseMauiReactorApp<MainPage8>(app =>
                 {
                     app.AddResource("Resources/Styles/Colors.xaml");
                     app.AddResource("Resources/Styles/Styles.xaml");
+                    app.AddResource("Resources/Styles/FixTabStyles.xaml");
                 })
 #if DEBUG
                 .EnableMauiReactorHotReload()
+                .OnMauiReactorUnhandledException(ex => 
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.ExceptionObject);
+                })
 #endif
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+                    fonts.AddFont("FontAwesomeRegular.otf", "FontRegular");
+                    fonts.AddFont("FontAwesomeBrands.otf", "FontBrands");
+                    fonts.AddFont("FontAwesomeSolid.otf", "FontSolid");
                 });
 
             return builder.Build();
---
+++ Controls/ShellTestPage/Pages/MainPage8.cs
@@ -0,0 +1,44 @@
+﻿using MauiReactor;
+using ShellTestPage.Resources.Styles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellTestPage.Pages;
+
+class MainPage8 : Component
+{
+    public override VisualNode Render()
+        => Shell(
+            TabBar(
+                Tab(
+                    ShellContent("Notifications")
+                        .RenderContent(() => new NotificationsPage())
+                )
+                .OnAndroid(_ => _.Icon(new MauiControls.FontImageSource 
+                    {
+                        FontFamily = "FontSolid",
+                        Glyph = AwesomeIconFont.Bell,
+                        Size = 20,
+                    }))
+                .OniOS(_=>_.Style(ResourceManager.FindStyle("FixIOSNotificationsTabStyle")))
+
+                .Title("Notifications"),
+
+                Tab(
+                    ShellContent()
+                        .RenderContent(() => new DatabasePage())
+                )
+                .OnAndroid(_ => _.Icon(new MauiControls.FontImageSource
+                {
+                    FontFamily = "FontSolid",
+                    Glyph = AwesomeIconFont.Database,
+                    Size = 20,
+                }))
+                .OniOS(_ => _.Style(ResourceManager.FindStyle("FixIOSDatabaseTabStyle")))
+
+                .Title("Database")
+            ));
+}
